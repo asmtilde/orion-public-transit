dbc05728c63e64d82bc6fd10dd4f7505c358e009
jack <alisamki@proton.me>
2025-06-19 22:51:56 -0700

    final stuff before sleepy time

--- src/engine/types.h
+++ src/engine/types.h
@@ -9,15 +9,36 @@
 #define TICKS_PER_SECOND 30
 #define MS_PER_TICK (1000 / TICKS_PER_SECOND)
 
+#define PALETTE_SIZE 256
 #define TILE_SIZE 32
 #define MAP_WIDTH (WINDOW_WIDTH / TILE_SIZE)
 #define MAP_HEIGHT (WINDOW_HEIGHT / TILE_SIZE)
 
-typedef struct {
+#define MAXIMUM_TEXT_LENGTH 512
+#define MAXIMUM_ACTORS 100
+
+#define PI 3.14159265358979323846
+
+typedef struct
+{
     Uint8 r;
     Uint8 g;
     Uint8 b;
 } Color;
+
+typedef struct
+{
+    Uint8 r;
+    Uint8 g;
+    Uint8 b;
+    Uint8 a;
+} Color_RGBA;
+
+typedef struct
+{
+    Color_RGBA colors[PALETTE_SIZE];
+    char *name[MAXIMUM_TEXT_LENGTH];
+} Palette;
 
 typedef struct
 {
@@ -27,20 +48,27 @@ typedef struct
     int tps;
 } App;
 
-typedef struct {
+typedef struct
+{
     int data [MAP_HEIGHT][MAP_WIDTH];
 } Map;
 
-struct Entity {
+typedef struct
+{
     int x;
     int y;
-    int facing;
-    SDL_Texture* texture;
-};
+    int width;
+    int height;
+} Bounding_Box;
 
-typedef struct {
-    struct Entity* entities;
-    int entity_count;
-} EntityManager;
+typedef struct 
+{
+    int x;
+    int y;
+    int width;
+    int height;
+    SDL_Texture* texture;
+    Bounding_Box bounding_box;
+} Actor;
 
 #endif
